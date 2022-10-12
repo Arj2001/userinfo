5e02437226af8788de5ae7f6e4c2524d5d3f5c9a
Arj2001 <75932871+Arj2001@users.noreply.github.com>
2022-10-12 15:42:23 +0530

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const addRoute = [
+  {
+    path:"",
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(addRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
